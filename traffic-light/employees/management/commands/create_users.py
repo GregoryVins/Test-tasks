64add22d7b604ca856fe66bf30c49d669270eee8
GregoryVins <gregoryvins@gmail.com>
2021-09-09 10:06:43 +0300

    decreased creating time of users

--- traffic-light/employees/management/commands/create_users.py
+++ traffic-light/employees/management/commands/create_users.py
@@ -52,57 +52,72 @@ class Command(BaseCommand):
     def get_or_create_administrators(self, num: int):
         admin_role, created = Role.objects.get_or_create(name='admin')
         owner_list = Employee.objects.all()
+        admin_list = []
 
         for _ in range(num):
             owner = choice(owner_list)
-            Employee.objects.get_or_create(username=self.create_random_username(USERNAME_LENGTH),
-                                           first_name=choice(NAMES),
-                                           position=admin_role,
-                                           salary=500,
-                                           supervisor=owner,
-                                           subdivision=owner.subdivision)
+            admin_list.append(Employee(username=self.create_random_username(USERNAME_LENGTH),
+                                       first_name=choice(NAMES),
+                                       position=admin_role,
+                                       salary=500,
+                                       supervisor=owner,
+                                       subdivision=owner.subdivision))
+
+        for item in admin_list:
+            item.save()
 
     def get_or_create_staff(self, num: int, usr_len: int):
         admin_role, created = Role.objects.get_or_create(name='admin')
         staff_role, created = Role.objects.get_or_create(name='staff')
         administrator_list = Employee.objects.filter(position=admin_role)
+        staff_list = []
 
         for _ in range(num):
             administrator = choice(administrator_list)
-            Employee.objects.get_or_create(username=self.create_random_username(usr_len),
-                                           first_name=choice(NAMES),
-                                           position=staff_role,
-                                           salary=250,
-                                           supervisor=administrator,
-                                           subdivision=administrator.subdivision)
+            staff_list.append(Employee(username=self.create_random_username(usr_len),
+                                       first_name=choice(NAMES),
+                                       position=staff_role,
+                                       salary=250,
+                                       supervisor=administrator,
+                                       subdivision=administrator.subdivision))
+        for item in staff_list:
+            item.save()
 
     def get_or_create_manager(self, num: int, usr_len: int):
         staff_role, created = Role.objects.get_or_create(name='staff')
         manager_role, created = Role.objects.get_or_create(name='manager')
         stall_list = Employee.objects.filter(position=staff_role)
+        manager_list = []
 
         for _ in range(num):
             administrator = choice(stall_list)
-            Employee.objects.get_or_create(username=self.create_random_username(usr_len),
-                                           first_name=choice(NAMES),
-                                           position=manager_role,
-                                           salary=125,
-                                           supervisor=administrator,
-                                           subdivision=administrator.subdivision)
+            manager_list.append(Employee(username=self.create_random_username(usr_len),
+                                         first_name=choice(NAMES),
+                                         position=manager_role,
+                                         salary=125,
+                                         supervisor=administrator,
+                                         subdivision=administrator.subdivision))
+
+        for item in manager_list:
+            item.save()
 
     def get_or_create_employee(self, num: int, usr_len: int):
         manager_role, created = Role.objects.get_or_create(name='manager')
         employee_role, created = Role.objects.get_or_create(name='employee')
         manager_list = Employee.objects.filter(position=manager_role)
+        employee_list = []
 
         for _ in range(num):
             administrator = choice(manager_list)
-            Employee.objects.get_or_create(username=self.create_random_username(usr_len),
-                                           first_name=choice(NAMES),
-                                           position=employee_role,
-                                           salary=62,
-                                           supervisor=administrator,
-                                           subdivision=administrator.subdivision)
+            employee_list.append(Employee(username=self.create_random_username(usr_len),
+                                          first_name=choice(NAMES),
+                                          position=employee_role,
+                                          salary=62,
+                                          supervisor=administrator,
+                                          subdivision=administrator.subdivision))
+
+        for item in employee_list:
+            item.save()
 
     def handle(self, *args, **options):
         with transaction.atomic():
